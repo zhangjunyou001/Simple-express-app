40af59e5aba5f55b261c2188c4ba853e6906a8ef
Ron <zhangjunyou001@126.com>
2023-09-16 10:53:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = request('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/',(req,res) => {res.sent('Hello World!')})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
